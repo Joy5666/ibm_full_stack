34af313ae6e233729d8b61e92279e4fe78b2de7a
joy5666 <joychakraborty328@gmail.com>
2025-02-20 22:42:51 +0530

    ass

---
+++ day8/classwork/index.js
@@ -0,0 +1,17 @@
+document.getElementById("parent").style.backgroundColor="blue"
+document.getElementById.style.border="1px soild black"
+
+let element = document.createElement("button")
+element.innerText="Submit"
+element.style.backgroundColor="gray"
+element.style.padding="10px"
+document.getElementById("parent").append(element)
+let num= 10;
+console.log(num)
+num=15;
+console.log(num)
+const num=44;z
+console.log(num)
+function myfunction(){
+    console.log(num)
+}
