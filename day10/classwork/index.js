ddb57c709d301e353e88f0f9069bc606dbc6cad0
joy5666 <joychakraborty328@gmail.com>
2025-02-24 20:59:32 +0530

    ass

---
+++ day10/classwork/index.js
@@ -0,0 +1,49 @@
+// {
+//     let a=10;
+//     console.log(a)
+// }
+// let a=10,b=20,c=30;
+// console.log(a+=a)
+// let a=10,b="10";
+// b=+b
+// if(a===+b){
+//     console.log("a is equal to b")
+// }
+// else{
+//     console.log("a is not equal to b")
+
+//  }
+// let a=10;
+// let b =20
+// myfunction(a,b)
+
+// // function myfunction (a,b){
+//     //     console.log(a+b)
+    
+//     // }
+//     const myfunction=(a,b)=>{
+//         console.log(a+b)
+//     }
+// console.log(name)
+// var name
+// console.log(a)
+
+// let a =10
+// myname()
+
+// console.log(a)
+// var a=10
+// function myname()
+// {
+    
+//     console.log(a)
+// }
+
+
+// let c ="     "
+// console.log(c.length)
+// let fnamne="sudip"
+// let lname="pradhan"
+// console.log(fnamne,lname)
+let b=20,c=+"30"
+console.log(b+c)
